58ece3f6603cb2c421d14fc30aa95489c32411d1
Mario Galan <mario.galan@gmail.com>
2017-06-15 11:00:55 +0200

    First version

---
+++ src/TestClass.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * This file is part of the project testpackage
+ * User: Mario Galán <mario.galan@gmail.com>
+ * Date: 15/06/17
+ * Time: 11:00
+ */
+
+namespace MarioGalan;
+
+
+/**
+ *
+ *
+ * @author Mario Galán <mario.galan@gmail.com>
+ */
+class TestClass
+{
+
+}
